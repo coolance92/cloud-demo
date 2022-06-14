c80d6d137e5e49995c88223c95e33d70ac81b5f3
coolance <liangxb@tele-zone.cn>
2022-06-14 21:21:52 +0800

    Eureka client服务发现

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,7 +3,10 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -11,6 +14,13 @@ public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.build();
     }
 
 }
