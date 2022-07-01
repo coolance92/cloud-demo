6048677e4cc8b54ce905c64711fbf80214342e10
coolance <liangxb@tele-zone.cn>
2022-07-01 11:20:30 +0800

    1、Feign的自定义配置

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package cn.itcast.order;
 
+import cn.itcast.order.config.FeignClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = FeignClientConfig.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
---
+++ order-service/src/main/java/cn/itcast/order/config/FeignClientConfig.java
@@ -0,0 +1,13 @@
+package cn.itcast.order.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class FeignClientConfig {
+
+    @Bean
+    public Logger.Level feignLogLevel() {
+        return Logger.Level.BASIC;
+    }
+
+}
