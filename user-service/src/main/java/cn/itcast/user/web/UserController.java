be7bec5193f4ffc83190216b1e606225f3221807
coolance <liangxb@tele-zone.cn>
2022-07-01 10:20:47 +0800

    1、nacos配置中心 2、配置热更新

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,37 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
+//@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("/now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
+    }
 
     /**
      * 路径： /user/110
